550870f9e1fff736d283ca1d948d894883050efa
Gaurav Sharma <gaurav91297@gmail.com>
2021-04-28 19:03:34 +0530

    some minor changes in login,register and password reset pages

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,10 @@ app_name = "core"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="core/home.html"), name="home_view"),
-    path("", TemplateView.as_view(template_name="accounts/login.html")),
-    path("", TemplateView.as_view(template_name="accounts/register.html")),
-    path("", TemplateView.as_view(template_name="accounts/forgot-password.html")),
+    path("login/", TemplateView.as_view(template_name="accounts/login.html")),
+    path("register/", TemplateView.as_view(template_name="accounts/register.html")),
+    path(
+        "forgot-password/",
+        TemplateView.as_view(template_name="accounts/forgot-password.html"),
+    ),
 ]
