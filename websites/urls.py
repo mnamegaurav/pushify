7531733c0bb819db98ddaa8dc73cfc443780e21a
Gaurav Sharma <gaurav91297@gmail.com>
2021-04-29 14:43:00 +0530

    added website list view

--- websites/urls.py
+++ websites/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from websites.views import WebsiteAddView, WebsiteEditView
+from websites.views import (
+    WebsiteAddView,
+    WebsiteEditView,
+    WebsitesListView,
+)
 
 app_name = "websites"
 
 urlpatterns = [
+    path("websites/", WebsitesListView.as_view(), name="websites_list_view"),
     path("website/add/", WebsiteAddView.as_view(), name="website_add_view"),
     path("website/edit/<int:pk>/", WebsiteEditView.as_view(), name="website_edit_view"),
 ]
--- websites/views.py
+++ websites/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, ListView
 from django.contrib.messages.views import SuccessMessageMixin
 
 from websites.models import Website
@@ -22,4 +22,10 @@ class WebsiteEditView(SuccessMessageMixin, UpdateView):
     pk_url_kwarg = "pk"
     extra_context = {"page_title": "Website Details"}
     success_message = "Successfully saved the changes"
+    queryset = Website.objects.filter(is_active=True)
+
+
+class WebsitesListView(ListView):
+    template_name = "websites/websites_list.html"
+    extra_context = {"page_title": "My Websites"}
     queryset = Website.objects.filter(is_active=True)
