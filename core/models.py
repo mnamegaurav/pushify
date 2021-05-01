ae43d0773928b9572bb61d772e90aa0fc13e66be
Gaurav Sharma <gaurav91297@gmail.com>
2021-05-01 17:29:23 +0530

    removed signals to save notification status, added celery state choices in notification model

--- core/models.py
+++ core/models.py
@@ -5,21 +5,19 @@ from django.urls import reverse_lazy
 
 from fcm_django.models import AbstractFCMDevice
 
+from celery import states
+
 from accounts.utils import auto_save_current_user
 
 from websites.models import Website
 
 User = get_user_model()
 
+ALL_STATES = sorted(states.ALL_STATES)
+TASK_STATE_CHOICES = sorted(zip(ALL_STATES, ALL_STATES))
+
 # Create your models here.
 class Notification(models.Model):
-    class StatusChoices(models.TextChoices):
-        SUCCESS = (1, "SUCCESS")
-        FAIL = (2, "FAILURE")
-        PENDING = (3, "PENDING")
-        STARTED = (4, "STARTED")
-        RETRY = (5, "RETRY")
-
     website = models.ForeignKey(
         Website, verbose_name=_("Website"), on_delete=models.CASCADE
     )
@@ -29,11 +27,12 @@ class Notification(models.Model):
     banner = models.ImageField(upload_to="notification_banner", null=True, blank=True)
     launch_url = models.URLField(max_length=500, null=True, blank=True)
     status = models.CharField(
-        max_length=2,
-        editable=False,
-        verbose_name=("Notification Status"),
-        choices=StatusChoices.choices,
-        default=StatusChoices.PENDING,
+        max_length=50,
+        default=states.PENDING,
+        db_index=True,
+        choices=TASK_STATE_CHOICES,
+        verbose_name=_("Notification Task Status"),
+        help_text=_("Current state of the notification task being run"),
     )
     created_by = models.ForeignKey(
         User,
